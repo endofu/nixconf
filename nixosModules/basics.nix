099a5533abb2982261374777b3df237a57cb272d
endofu <endofu@gmail.com>
2024-08-26 19:47:09 +0000

    Feat: Add starship

--- nixosModules/basics.nix
+++ nixosModules/basics.nix
@@ -13,5 +13,22 @@
     programs.zsh.enable = true;
     users.defaultUserShell = pkgs.zsh;
 
+    programs.starship = {
+      enable = true;
+      settings = {
+        add_newline = false;
+        format = lib.concatStrings [
+          "$line_break"
+          "$package"
+          "$line_break"
+          "$character"
+        ];
+        scan_timeout = 10;
+        character = {
+          success_symbol = "➜";
+          error_symbol = "➜";
+        };
+      };
+    };
   };
 }
