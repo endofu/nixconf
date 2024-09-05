98735890f2d5aa7b7268c3852c878d1c1c0c2c35
endofu <endofu@gmail.com>
2024-09-05 16:40:23 +0200

    Feat: Add anydesk to elaine

--- hosts/elaine/configuration.nix
+++ hosts/elaine/configuration.nix
@@ -153,6 +153,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     autorandr
+    anydesk
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
   #  wget
   ];
