722ba6b21643bdcb6ae95cc5f382f5298cf955fb
endofu <endofu@gmail.com>
2025-03-11 22:20:33 +0100

    Feat: Config TMUX and Zed

--- darwinModules/devtools.nix
+++ darwinModules/devtools.nix
@@ -1,9 +1,6 @@
 { lib, config, ... }: {
 
-  options = {
-    devtools.enable =
-      lib.mkEnableOption "enables devtools bundle";
-  };
+  options = { devtools.enable = lib.mkEnableOption "enables devtools bundle"; };
 
   config = lib.mkIf config.devtools.enable {
 
@@ -11,12 +8,41 @@
       programs = {
         zed-editor = {
           enable = true;
-          extensions = ["nix"];
+          extensions = [ "nix" "toml" ];
+          userSettings = {
+            hour_format = "hour24";
+            auto_update = false;
+            telemetry = { metrics = false; };
+            vim_mode = true;
+            autosave = { after_delay = { milliseconds = 1000; }; };
+            formatter = "language_server";
+            format_on_save = true;
+            lsp = {
+              rust-analyzer = {
+
+                binary = {
+                  #                        path = lib.getExe pkgs.rust-analyzer;
+                  path_lookup = true;
+                };
+              };
+              nix = { binary = { path_lookup = true; }; };
+              nil = {
+                initialization_options = {
+                  formatting = { command = [ "nixfmt" ]; };
+                  nix = { flake = { autoArchive = true; }; };
+                };
+              };
+            };
+            theme = {
+              mode = "system";
+              light = "One Light";
+              dark = "One Dark";
+            };
+
+          };
         };
 
-        lazygit = {
-          enable = true;
-        };
+        lazygit = { enable = true; };
       };
     };
   };
--- darwinModules/terminal.nix
+++ darwinModules/terminal.nix
@@ -1,14 +1,20 @@
-{ pkgs, lib, config, ... }: {
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}:
+{
 
   options = {
-    terminal.enable =
-      lib.mkEnableOption "enables terminal bundle";
+    terminal.enable = lib.mkEnableOption "enables terminal bundle";
   };
 
   config = lib.mkIf config.terminal.enable {
     environment.systemPackages = with pkgs; [
       nixd
       nil
+      nixfmt-rfc-style
       mc
       bat
       eza
@@ -36,67 +42,66 @@
           terminal = "tmux-256color";
 
           extraConfig = ''
-              # set-option -g default-terminal 'screen-256color'
-              set-option -g terminal-overrides ',xterm-256color:RGB'
-              set -gu default-command
+                          # set-option -g default-terminal 'screen-256color'
+                          set-option -g terminal-overrides ',xterm-256color:RGB'
+                          set -gu default-command
 
-              set -g detach-on-destroy off     # don't exit from tmux when closing a session
-              set -g renumber-windows on       # renumber all windows when any window is closed
-              set -g set-clipboard on          # use system clipboard
-              set -g status-position top       # macOS / darwin style
-              setw -g mode-keys vi
-              set -g pane-active-border-style 'fg=magenta,bg=default'
-              set -g pane-border-style 'fg=brightblack,bg=default'
+                          set -g detach-on-destroy off     # don't exit from tmux when closing a session
+                          set -g renumber-windows on       # renumber all windows when any window is closed
+                          set -g set-clipboard on          # use system clipboard
+                          set -g status-position top       # macOS / darwin style
+                          setw -g mode-keys vi
+                          set -g pane-active-border-style 'fg=magenta,bg=default'
+                          set -g pane-border-style 'fg=brightblack,bg=default'
 
-#             # set -g @fzf-url-fzf-options '-p 60%,30% --prompt="   " --border-label=" Open URL "'
-#             # set -g @fzf-url-history-limit '2000'
+            #             # set -g @fzf-url-fzf-options '-p 60%,30% --prompt="   " --border-label=" Open URL "'
+            #             # set -g @fzf-url-history-limit '2000'
 
-#             bind s choose-session
-        '';
+            #             bind s choose-session
+          '';
 
-          plugins =
-          with pkgs; [
+          plugins = with pkgs; [
             # tmuxPlugins.sensible
             tmuxPlugins.copycat
             tmuxPlugins.yank
             # must be before continuum edits right status bar
             {
-                plugin = tmuxPlugins.catppuccin;
-                extraConfig = ''
-                  set -g @catppuccin_window_left_separator ""
-                  set -g @catppuccin_window_right_separator " "
-                  set -g @catppuccin_window_middle_separator "█"
-                  set -g @catppuccin_window_number_position "left"
-                  set -g @catppuccin_window_default_fill "number"
-                  set -g @catppuccin_window_default_text " #W"
-                  set -g @catppuccin_window_current_fill "number"
-                  set -g @catppuccin_window_current_text " #W#{?window_zoomed_flag,(),}"
-                  set -g @catppuccin_status_modules_right "directory meetings date_time"
-                  set -g @catppuccin_status_modules_left "session"
-                  set -g @catppuccin_status_left_separator  " "
-                  set -g @catppuccin_status_right_separator " "
-                  set -g @catppuccin_status_right_separator_inverse "no"
-                  set -g @catppuccin_status_fill "icon"
-                  set -g @catppuccin_status_connect_separator "no"
-                  set -g @catppuccin_directory_text "#{b:pane_current_path}"
-                  set -g @catppuccin_date_time_text "%H:%M"
-                '';
+              plugin = tmuxPlugins.catppuccin;
+              extraConfig = ''
+                set -g @catppuccin_window_left_separator ""
+                set -g @catppuccin_window_right_separator " "
+                set -g @catppuccin_window_middle_separator "█"
+                set -g @catppuccin_window_number_position "left"
+                set -g @catppuccin_window_default_fill "number"
+                set -g @catppuccin_window_default_text " #W"
+                set -g @catppuccin_window_current_fill "number"
+                set -g @catppuccin_window_current_text " #W#{?window_zoomed_flag,(),}"
+                set -g @catppuccin_status_modules_right "directory meetings date_time"
+                set -g @catppuccin_status_modules_left "session"
+                set -g @catppuccin_status_left_separator  " "
+                set -g @catppuccin_status_right_separator " "
+                set -g @catppuccin_status_right_separator_inverse "no"
+                set -g @catppuccin_status_fill "icon"
+                set -g @catppuccin_status_connect_separator "no"
+                set -g @catppuccin_directory_text "#{b:pane_current_path}"
+                set -g @catppuccin_date_time_text "%H:%M"
+              '';
             }
             {
-                plugin = tmuxPlugins.resurrect;
-                extraConfig = ''
+              plugin = tmuxPlugins.resurrect;
+              extraConfig = ''
                 set -g @resurrect-strategy-vim 'session'
                 set -g @resurrect-strategy-nvim 'session'
                 set -g @resurrect-capture-pane-contents 'on'
-                '';
+              '';
             }
             {
-                plugin = tmuxPlugins.continuum;
-                extraConfig = ''
+              plugin = tmuxPlugins.continuum;
+              extraConfig = ''
                 set -g @continuum-restore 'on'
                 # set -g @continuum-boot 'on'
                 set -g @continuum-save-interval '10'
-                '';
+              '';
             }
 
             tmuxPlugins.tmux-thumbs
