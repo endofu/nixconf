1309997a3271494b4019f7e14dc9a6b0a6b69f66
endofu <endofu@gmail.com>
2025-03-19 11:43:26 +0100

    Fix: Tmux shell to run nix version of zsh

--- darwinModules/terminal.nix
+++ darwinModules/terminal.nix
@@ -37,6 +37,7 @@
       ls = "lsd -al";
       lst = "lsd -al --tree";
       cat = "bat";
+      nvim = "nix run ~/Code/nixconf/nvf"; # this should be referenced relatively
     };
 
     home-manager.users.arcadia = {
@@ -80,10 +81,12 @@
           baseIndex = 1;
           newSession = true;
           terminal = "tmux-256color";
+          shell = "${pkgs.zsh}/bin/zsh";
 
           extraConfig = ''
             set-option -g terminal-overrides ',xterm-256color:RGB'
             set -gu default-command
+            # set -g default-command zsh
             set -g detach-on-destroy off     # don't exit from tmux when closing a session
             set -g renumber-windows on       # renumber all windows when any window is closed
             set -g set-clipboard on          # use system clipboard
@@ -97,29 +100,6 @@
             # tmuxPlugins.sensible
             tmuxPlugins.copycat
             tmuxPlugins.yank
-            # must be before continuum edits right status bar
-            # {
-            #   plugin = tmuxPlugins.catppuccin;
-            #   extraConfig = ''
-            #     set -g @catppuccin_window_left_separator ""
-            #     set -g @catppuccin_window_right_separator " "
-            #     set -g @catppuccin_window_middle_separator "█"
-            #     set -g @catppuccin_window_number_position "left"
-            #     set -g @catppuccin_window_default_fill "number"
-            #     set -g @catppuccin_window_default_text " #W"
-            #     set -g @catppuccin_window_current_fill "number"
-            #     set -g @catppuccin_window_current_text " #W#{?window_zoomed_flag,(),}"
-            #     set -g @catppuccin_status_modules_right "directory meetings date_time"
-            #     set -g @catppuccin_status_modules_left "session"
-            #     set -g @catppuccin_status_left_separator  " "
-            #     set -g @catppuccin_status_right_separator " "
-            #     set -g @catppuccin_status_right_separator_inverse "no"
-            #     set -g @catppuccin_status_fill "icon"
-            #     set -g @catppuccin_status_connect_separator "no"
-            #     set -g @catppuccin_directory_text "#{b:pane_current_path}"
-            #     set -g @catppuccin_date_time_text "%H:%M"
-            #   '';
-            # }
             {
               plugin = tmuxPlugins.tokyo-night-tmux;
               extraConfig = ''
