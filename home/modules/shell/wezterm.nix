22041433acddb02541e1675649d10bb66fd4d87a
endofu <endofu@gmail.com>
2025-04-10 11:36:24 +0200

    Fix: Make Wezterm wayland-compatible

--- home/modules/shell/wezterm.nix
+++ home/modules/shell/wezterm.nix
@@ -1,10 +1,15 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  ...
+}:
 
 with lib;
 
 let
   cfg = config.modules.shell.wezterm;
-in {
+in
+{
   options.modules.shell.wezterm = {
     enable = mkEnableOption "wezterm configuration";
 
@@ -13,22 +18,24 @@ in {
   config = mkIf cfg.enable {
 
     programs = {
-        wezterm = {
-            enable = true;
-            extraConfig = ''
-                return {
-                window_background_opacity = 0.8,
-                macos_window_background_blur = 20,
-                font = wezterm.font("BlexMono Nerd Font Mono"),
-                font_size = 14.0,
-                cell_width = 0.9,
-                color_scheme = "Tokyo Night Storm",
-                native_macos_fullscreen_mode = true,
-                hide_tab_bar_if_only_one_tab = true,
-                window_decorations = "RESIZE"
-                }
-            '';
-        };
+      wezterm = {
+        enable = true;
+        extraConfig = ''
+          return {
+            window_background_opacity = 0.9,
+            macos_window_background_blur = 20,
+            font = wezterm.font("BlexMono Nerd Font Mono"),
+            font_size = 14.0,
+            cell_width = 0.9,
+            color_scheme = "Tokyo Night Storm",
+            hide_tab_bar_if_only_one_tab = true,
+            window_decorations = "NONE",
+
+            enable_wayland = true, 
+            native_macos_fullscreen_mode = true,
+          }
+        '';
+      };
     };
   };
 }
