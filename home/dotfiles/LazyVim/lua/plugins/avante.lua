3203ca38d3fdca69328172b37f3707a3023228a5
endofu <endofu@gmail.com>
2025-06-25 00:06:43 +0200

    Fix: Migrate avante providers syntax

--- home/dotfiles/LazyVim/lua/plugins/avante.lua
+++ home/dotfiles/LazyVim/lua/plugins/avante.lua
@@ -6,12 +6,17 @@ return {
   opts = {
     -- add any opts here
     -- for example
-    provider = "claude",
-    claude = {
-      endpoint = "https://api.anthropic.com",
-      model = "claude-3-5-sonnet-20241022",
-      temperature = 0,
-      max_tokens = 4096,
+    providers = {
+      provider = "claude",
+      claude = {
+        endpoint = "https://api.anthropic.com",
+        model = "claude-sonnet-4-20250514",
+        timeout = 120000,
+        extra_request_body = {
+          temperature = 0,
+          max_tokens = 4096,
+        },
+      },
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
