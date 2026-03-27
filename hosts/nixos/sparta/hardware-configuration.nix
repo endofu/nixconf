f0966454919e90b85b4952272e51eecc7090a059
endofu <endofu@gmail.com>
2026-03-27 12:06:48 +0100

    Fix: Upgrade Broadcom on sparta (+flake update)

--- hosts/nixos/sparta/hardware-configuration.nix
+++ hosts/nixos/sparta/hardware-configuration.nix
@@ -14,7 +14,7 @@
   ];
 
   nixpkgs.config.permittedInsecurePackages = [
-    "broadcom-sta-6.30.223.271-59-6.12.65"
+    "broadcom-sta-6.30.223.271-59-6.18.19"
   ];
 
   boot.initrd.availableKernelModules = [
